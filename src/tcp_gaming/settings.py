a8a67549d36f89e29f0831d3aee13244d38654fd
xdJidx <bernardjerome59@gmail.com>
2023-11-06 11:13:17 +0100

    Use staticfiles repo in django

--- src/tcp_gaming/settings.py
+++ src/tcp_gaming/settings.py
@@ -142,6 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "tcp_gaming/static")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
